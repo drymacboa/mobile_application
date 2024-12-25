c740f2f08816ac5cbc393b030524c797d6d676ce
drymacboa <drymacboa@gmail.com>
2024-12-25 14:04:07 +0200

    Solving errors in AndroidManifest.xml

--- app/src/main/java/com/example/assignment_mob_app/CreateAccountActivity.kt
+++ app/src/main/java/com/example/assignment_mob_app/CreateAccountActivity.kt
@@ -1,11 +1,22 @@
+package com.example.assignment_mob_app
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.yourappname.R
+import com.example.assignment_mob_app.R
+import com.example.assignment_mob_app.SignInActivity
 
 class CreateAccountActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_account)
+        setContentView(R.layout.create_acc)
+        // Navigate to SignInActivity
+        findViewById<TextView>(R.id.tvAlreadyMember).setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
